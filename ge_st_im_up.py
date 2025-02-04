3155230b068535a34ecd21ccadd311b68ac41e9d
rabique <oddyseibcm@gmail.com>
2025-02-04 10:52:21 +0900

    1st_file

---
+++ ge_st_im_up.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from PIL import Image
+import google.generativeai as genai
+
+
+st.title("image 파일 업로드")
+st.subheader("1. 컴퓨터에 있는 이미지 파일을 업로드")
+
+uploaded_file = st.file_uploader("Image file 선택 " , type = None)
+
+with st.spinner('잠시만 기다려주세요...'):
+    
+    if uploaded_file:
+        image = Image.open(uploaded_file)
+        image.save("output_image.jpg")
+        st.image("output_image.jpg")
+        
+        st.image(image, width=350, caption='컴퓨터에 있는 이미지 파일 표시')
+        
+        genai.configure()
+        model = genai.GenerativeModel("gemini-2.0-flash-exp")
+        
+        response = model.generate_content(["Tell me about this photo in korean", image])
+        st.write(response.text)
+st.success('Done!')
